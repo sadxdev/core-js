310dec4c6129666a9e2a0bdfb11c478dbcf002e8
sadxdev <sadikxdev@gmail.com>
2026-01-30 23:31:50 +0530

    objects

---
+++ objects.js
@@ -0,0 +1,5 @@
+// objects = A collection of related properties and/or methods
+//          can represent real world objects (people, products, places)
+//          object = {key:value, function}
+
+
